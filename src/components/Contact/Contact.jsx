1e994712e8206b1fc4d45d7807afa91391e62680
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-05-05 14:46:06 +0200

    css-contacts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,19 @@
 import css from "./Contact.module.css";
+import { ImAddressBook } from "react-icons/im";
+import { ImPhone } from "react-icons/im";
 
 const Contact = ({ name, number, onDelete }) => {
   return (
     <li className={css.item}>
       <div className={css.grup}>
-        <p className={css.name}> {name}</p>
-        <p className={css.number}>📞{number}</p>
+        <p className={css.name}>
+          <ImAddressBook className={css.icon} />
+          {name}
+        </p>
+        <p className={css.number}>
+          <ImPhone className={css.icon} />
+          {number}
+        </p>
       </div>
       <button className={css.button} onClick={onDelete}>
         Delete
